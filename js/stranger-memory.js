71b56a154f51eeeacae484394826edbdbd1a9783
crystalfinch <crystal.storm@gmail.com>
2016-08-14 14:50:18 -0700

    Default settings

--- js/stranger-memory.js
+++ js/stranger-memory.js
@@ -29,6 +29,103 @@
 		// do stuff
 	}
 
+	// default settings
+	StrangerMemory.prototype.settings = {
+		wrapper : "sm-wrapper",
+		cards : [
+			{
+				id: 1,
+				name: "Barb",
+				image: "images/barb.jpg"
+			},
+			{
+				id: 2,
+				name: "Chief Hopper",
+				image: "images/chief-hopper.jpg"
+			},
+			{
+				id: 3,
+				name: "Connie",
+				image: "images/connie.jpg"
+			},
+			{
+				id: 4,
+				name: "Dr. Brenner",
+				image: "images/dr-brenner.jpg"
+			},
+			{
+				id: 5,
+				name: "Dustin",
+				image: "images/dustin.jpg"
+			},
+			{
+				id: 6,
+				name: "Eggos",
+				image: "images/eggos.jpg"
+			},
+			{
+				id: 7,
+				name: "Eleven",
+				image: "images/eleven.jpg"
+			},
+			{
+				id: 8,
+				name: "Jonathan",
+				image: "images/jonathan.jpg"
+			},
+			{
+				id: 9,
+				name: "Joyce",
+				image: "images/joyce.jpg"
+			},
+			{
+				id: 10,
+				name: "Karen",
+				image: "images/karen.jpg"
+			},
+			{
+				id: 11,
+				name: "Lucas",
+				image: "images/lucas.jpg"
+			},
+			{
+				id: 12,
+				name: "Mike",
+				image: "images/mike.jpg"
+			},
+			{
+				id: 13,
+				name: "The Monster",
+				image: "images/monster.jpg"
+			},
+			{
+				id: 14,
+				name: "Mr. Clarke",
+				image: "images/mr-clarke.jpg"
+			},
+			{
+				id: 15,
+				name: "Nancy",
+				image: "images/nancy.jpg"
+			},
+			{
+				id: 16,
+				name: "Steve",
+				image: "images/steve.jpg"
+			},
+			{
+				id: 17,
+				name: "Troy",
+				image: "images/troy.jpg"
+			},
+			{
+				id: 18,
+				name: "Will",
+				image: "images/will.jpg"
+			}
+		]
+	}
+
 	// add StrangerMemory to global namespace
 	window.StrangerMemory = StrangerMemory;
 
