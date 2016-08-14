a4401b9c811a2e07cc62be3a284298f76ff4c19c
crystalfinch <crystal.storm@gmail.com>
2016-08-14 11:49:20 -0700

    Set up index & asset files

---
+++ js/stranger-memory.js
@@ -0,0 +1,14 @@
+// create private scope
+;(function( window ) {
+	'use strict';
+
+	function StrangerMemory( settings ) {
+		// do stuff
+	}
+
+	// add StrangerMemory to global namespace
+	window.StrangerMemory = StrangerMemory;
+
+})( window );
+
+var smGame = new StrangerMemory;
