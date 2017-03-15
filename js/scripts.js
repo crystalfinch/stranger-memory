1883f4a97a18339123454e57c6ce6494ea89636a
Crystal Finch <crystal.storm@gmail.com>
2017-03-15 12:23:26 -0700

    Remove music widget

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,6 @@ function ready(fn) {
 
 ready(function() {
 	var smGame = new StrangerMemory;
-	var popoverToggle = document.querySelector(".music-widget .trigger");
-	var popoverClose = document.querySelector(".music-widget .close");
-
-	popoverToggle.addEventListener('click', function(e) {
-		e.preventDefault();
-		if (this.parentNode.classList.contains('in')) {
-			this.parentNode.classList.remove('in');
-		} else {
-			this.parentNode.classList.add('in');
-		}
-	});
-
-	popoverClose.addEventListener('click', function(e) {
-		e.preventDefault();
-		if (document.querySelector(".music-widget").classList.contains('in')) {
-			document.querySelector(".music-widget").classList.remove('in');
-		} else {
-			document.querySelector(".music-widget").add('in');
-		}
-	});
 
 	// when images are loaded
 	window.onload = function() {
